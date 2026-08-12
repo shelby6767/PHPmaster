4c8a6627e462406c6140a04f0174bcff59f6bc75
shelby <afiqmijan2@gmail.com>
2026-08-12 19:02:31 +0800

    dont really understand this project so far but we'll get there

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+const INCLUDES_DIR = __DIR__ . '/../includes';
+const ROUTES_DIR = __DIR__ . '/../routes';
+
+session_start();
+require_once INCLUDES_DIR . '/router.php';
+
+dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);

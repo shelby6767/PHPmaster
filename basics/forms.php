95bb5160469777b96edccee03c24e372bcab1944
shelby <afiqmijan2@gmail.com>
2026-08-12 18:34:52 +0800

    restruct folder

---
+++ basics/forms.php
@@ -0,0 +1,18 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        echo "The email $email was submitted";
+        die;
+    }
+?>
+
+<html>
+<body>
+    <h1>Please Submit The Form</h1>
+    <form method ="POST">
+        <label>Email:</label>
+        <input type="email" name="email">
+        <button>Submit</button>
+    </form>
+</body>
+</html>
